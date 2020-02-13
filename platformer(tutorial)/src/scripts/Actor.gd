224ce0e86f41e13545c7a00fdebb88cbfc5521f2
unknown <nelsondiasdemedeiros@gmail.com>
2020-02-13 17:26:28 -0200

    feature: add physic to the player

---
+++ platformer(tutorial)/src/scripts/Actor.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+class_name Actor
+
+# up: Vector2(0, -1)
+const FLOOR_NORMAL = Vector2(0, -1)
+
+var speed = Vector2(800.0, 1400.0)
+var velocity = Vector2(0, 0)
+var gravity = 4000.0
