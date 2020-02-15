f76352cc8278b6ca8949fff4ec9b2c8eaf1a0a69
unknown <nelsondiasdemedeiros@gmail.com>
2020-02-15 13:18:49 -0200

    refactor: Moviments refactor of player and anemy

--- platformer(tutorial)/src/scenes/player/player.gd
+++ platformer(tutorial)/src/scenes/player/player.gd
@@ -23,27 +23,22 @@ func _physics_process(delta):
 func get_direction():
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump") else 0.0
 	)
 	
 	
 func calc_move_velocity(linear_velocity, direction, speed, is_jump_interrupted):
 	var out = linear_velocity
 	out.x = speed.x * direction.x
-	out.y += self.gravity * get_physics_process_delta_time()
-	if direction.y == -1.0: 
+	if direction.y != 0.0: 
 		out.y = speed.y * direction.y
 	if is_jump_interrupted: 
 		out.y = 0.0
 	return out
-	
-	
+
+
 func calc_stomp_velocity(linear_velocity, impulce):
 	var out = linear_velocity
 	out.y = -impulce
 	return out
 	
-	
-func calculate_stomp_velocity(linear_velocity, stomp_impulse):
-	var stomp_jump = -self.speed.y if Input.is_action_pressed("jump") else -stomp_impulse
-	return Vector2(linear_velocity.x, stomp_jump)
