73d09bb618ec95c77d36975170dd69fbc10d44cb
unknown <nelsondiasdemedeiros@gmail.com>
2020-02-16 14:17:34 -0300

    refactor: refactored name of scenes from levels

--- platformer(tutorial)/src/screens/start/play_button.gd
+++ platformer(tutorial)/src/screens/start/play_button.gd
@@ -2,4 +2,4 @@ extends Button
 
 
 func _on_play_button_up():
-	get_tree().change_scene("res://src/scenes/leveltemplete/level01.tscn")
+	get_tree().change_scene("res://src/scenes/levels/level01.tscn")
